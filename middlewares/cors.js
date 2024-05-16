3319ccc216bfe648829d43e194bcc7032a6f72a9
roso1nik <103901662+roso1nik@users.noreply.github.com>
2024-05-16 19:32:37 +0300

    fix cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -6,11 +6,20 @@ function cors(req, res, next) {
         "https://students-projects.ru",
         "http://localhost:3000",
         "http://localhost:3001",
+        "https://pindie.nomoredomainswork.ru",
     ];
 
-    if (allowedCors.includes(origin))
+    if (allowedCors.includes(origin)) {
         res.header("Access-Control-Allow-Origin", origin);
-
+    }
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,PUT,PATCH,POST,DELETE"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+    );
     next();
 }
 
